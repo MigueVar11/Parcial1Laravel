1ba8b10a0a99201a6deecbddd8c522773737ef4f
Miguel Vargas <91300170+MigueVar11@users.noreply.github.com>
2024-10-21 23:51:34 -0500

    SegCommit

--- peliculas-api/routes/api.php
+++ peliculas-api/routes/api.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PeliculaController;
 
 Route::post('/login', [LoginController::class, 'login']);
-Route::post('/register', [LoginController::class, 'register']);
-Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/register', [LoginController::class, 'register'])->middleware('auth:sanctum');
+
 
 Route::apiResource('peliculas', PeliculaController::class)
     ->middleware('auth:sanctum');
